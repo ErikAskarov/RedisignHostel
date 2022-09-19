62c27c592b7636d0a0f5dc170e78134af5857fd0
ErikAskarov <91485464+ErikAskarov@users.noreply.github.com>
2022-09-19 13:36:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path-browserify'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/RedisignHostel/',
   plugins: [vue()],
   resolve: {
     alias: {
       path: "path-browserify",
-      base: '/RedisignHostel/'
     },
   },
 })
